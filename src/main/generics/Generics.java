f3da29ee39fee2fdbb40a05bace7d08777d94191
subhamstark <subham@sap.com>
2020-10-06 21:36:06 +0530

    UC2 Done

--- src/main/generics/Generics.java
+++ src/main/generics/Generics.java
@@ -1,18 +1,6 @@
 package main.generics;
 
 public class Generics {
-
-//    public static void main(String[] args) {
-//        Integer array1[] = {3, 2, 1};
-//        findMax(array1);
-//
-//        Integer array2[] = {1, 3, 2};
-//        findMax(array1);
-//
-//        Integer array3[] = {1, 2, 3};
-//        findMax(array1);
-//    }
-
     public Integer findMax(Integer[] array) {
         Integer maxInt = array[0];
         if(array[1].compareTo(maxInt)>0)
@@ -22,5 +10,24 @@ public class Generics {
 
         System.out.println(maxInt);
         return maxInt;
+    }
+    public Float findMaximumFloat(Float[] numbers) {
+        Float maxFloat=numbers[0];
+        if(numbers[1].compareTo(maxFloat)>0)
+            maxFloat=numbers[1];
+        if(numbers[2].compareTo(maxFloat)>0)
+            maxFloat=numbers[2];
+
+        return maxFloat;
+    }
+
+    public String findMaximumString(String[] numbers) {
+        String maxString=numbers[0];
+        if(numbers[1].compareTo(maxString)>0)
+            maxString=numbers[1];
+        if(numbers[2].compareTo(maxString)>0)
+            maxString=numbers[2];
+
+        return maxString;
     }
 }
