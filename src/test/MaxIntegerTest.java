3ae630eea0810741ba742afbfb8f6d19aee646b2
subhamstark <subham@sap.com>
2020-10-06 21:54:41 +0530

    Refactor1 Done

--- src/test/MaxIntegerTest.java
+++ src/test/MaxIntegerTest.java
@@ -17,45 +17,45 @@ public class MaxIntegerTest {
     // Integer Test
     @Test
     public void MaxAt1stPositionInt() {
-        Assert.assertEquals(new Integer(3), max.findMax(new Integer[] {3,2,1}));
+        Assert.assertEquals(new Integer(3), max.findMaxGeneric(new Integer[] {3,2,1}));
 
     }
     @Test
     public void MaxAt2stPositionInt() {
-        Assert.assertEquals(new Integer(4), max.findMax(new Integer[] {3,4,1}));
+        Assert.assertEquals(new Integer(4), max.findMaxGeneric(new Integer[] {3,4,1}));
 
     }
     @Test
     public void MaxAt3stPositionInt() {
-        Assert.assertEquals(new Integer(5), max.findMax(new Integer[] {3,4,5}));
+        Assert.assertEquals(new Integer(5), max.findMaxGeneric(new Integer[] {3,4,5}));
 
     }
 
     // Float Test
     @Test
     public void MaxAt1stPositionFloat() {
-        Assert.assertEquals(new Float(3f), max.findMaximumFloat(new Float[] {3f,2f,1f}));
+        Assert.assertEquals(new Float(3f), max.findMaxGeneric(new Float[] {3f,2f,1f}));
     }
     @Test
     public void MaxAt2stPositionFloat() {
-        Assert.assertEquals(new Float(4f), max.findMaximumFloat(new Float[] {3f,4f,1f}));
+        Assert.assertEquals(new Float(4f), max.findMaxGeneric(new Float[] {3f,4f,1f}));
     }
     @Test
     public void MaxAt3stPositionFloat() {
-        Assert.assertEquals(new Float(5f), max.findMaximumFloat(new Float[] {3f,4f,5f}));
+        Assert.assertEquals(new Float(5f), max.findMaxGeneric(new Float[] {3f,4f,5f}));
     }
 
     // String test
     @Test
     public void givenMaxString_At1stPosition_ReturnString() {
-        Assert.assertEquals(new String("Mango"), max.findMaximumString(new String[] {"Mango","Banana","Apple"}));
+        Assert.assertEquals(new String("Mango"), max.findMaxGeneric(new String[] {"Mango","Banana","Apple"}));
     }
     @Test
     public void givenMaxString_At2ndPosition_ReturnString() {
-        Assert.assertEquals(new String("Grapefruit"), max.findMaximumString(new String[] {"Cherries","Grapefruit","Apple"}));
+        Assert.assertEquals(new String("Grapefruit"), max.findMaxGeneric(new String[] {"Cherries","Grapefruit","Apple"}));
     }
     @Test
     public void givenMaxString_At3rdPosition_ReturnString() {
-        Assert.assertEquals(new String("Kiwi"), max.findMaximumString(new String[] {"Cherries","Banana","Kiwi"}));
+        Assert.assertEquals(new String("Kiwi"), max.findMaxGeneric(new String[] {"Cherries","Banana","Kiwi"}));
     }
 }
